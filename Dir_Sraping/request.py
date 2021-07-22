cb81f1eadd4ea0de5608607b7f3f20c33576cc91
Ajang elvis Ngwesse <NSNCareers@outlook.com>
2021-07-22 03:33:56 +0100

    updates

---
+++ Dir_Sraping/request.py
@@ -0,0 +1,18 @@
+import requests as rq
+from requests.auth import HTTPBasicAuth
+import  sys
+sys.path.append('/Users/ajang/Desktop/Repositories/DataScience')
+import Dir_Sraping.disableRequest as sb
+
+loginUrl = 'https://www.trademap.org/Index.aspx'
+username = 'v_diwan@ssseurope.onmicrosoft.com'
+passwrod = 'ywrUt5ZWj6#5iX!'
+
+
+results = rq.get(loginUrl,auth=HTTPBasicAuth(username,passwrod),verify=False)
+
+statusCode = results.status
+data = results.text
+
+print(data)
+print()
---
+++ Dir_Sraping/scraping1.py
@@ -0,0 +1,56 @@
+
+#  pip install beautifulsoup4
+# pip install lxml (we shall be using this)
+# pip instal html5lib
+# pip install request
+
+from bs4 import BeautifulSoup
+import  requests as rq 
+import csv
+
+# open a file using context manager
+#with open('Dir_Sraping\scaping.csv','w') as csv_file:
+    #csv_writer = csv.writer(csv_file)
+    #csv_writer.writerow(['Title','Tag'])
+
+csv_file = open('Dir_Sraping\scaping.csv','w')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Title','Tag'])
+
+
+def getContent():
+    results = rq.get('http://qa.loginapp.nsncareers.com/') 
+    soup = BeautifulSoup(results.text,'lxml') # lxml parser helps to parse xml and html contenet
+    # content = soup.prettify() # Helps to indent
+    # content = soup.title # Gets page title
+    title = soup.title.text # Gets only title withot html tags
+    # div = soup.div # Gets the first dive tag of the page
+    # content = soup.find('a', {'class':'navbar-brand'}) # get tag a with class 
+
+    # container = soup.find('div', {'class':'navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse'}) 
+    # text = container.ul.li.a.text # Get text using child tag
+
+    content = soup.find_all('div', {'class':'container'}) # returns a list of all the specified elements
+
+    # We can loop through
+    #for x in content:
+        #content1 = x.h1
+        #content2 = x.a
+        #print(content1)
+        #print(content2)
+    
+
+    # Get specific tag by using dictionary object
+    href_list = soup.find('li', {'class':'nav-item'}).a['href']
+    # Split list
+    split_list = href_list.split('/')
+    # Get 4 th index
+    content = split_list[3]
+    csv_writer.writerow([title,content])
+    csv_file.close()
+    # print(content)
+
+
+getContent()
+
+
