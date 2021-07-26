728168cff0c47231492de48fbde0eaecf14636bd
Ajang elvis Ngwesse <NSNCareers@outlook.com>
2021-07-26 23:00:33 +0100

    updates

--- Dir_Sraping/request.py
+++ Dir_Sraping/request.py
@@ -7,9 +7,10 @@ import Dir_Sraping.disableRequest as sb
 loginUrl = 'https://www.trademap.org/Index.aspx'
 username = 'v_diwan@ssseurope.onmicrosoft.com'
 passwrod = 'ywrUt5ZWj6#5iX!'
+token = "XYCKsqjUT2l7_-UKDMrL4eOhoN2hTHHP1_gS500Zuf0"
 
-
-results = rq.get(loginUrl,auth=HTTPBasicAuth(username,passwrod),verify=False)
+# results = rq.get(loginUrl,auth=HTTPBasicAuth(username,passwrod),verify=False)
+results = rq.get(loginUrl,headers={'Authorization': token})
 
 statusCode = results.status
 data = results.text
